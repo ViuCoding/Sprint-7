ddf1c8595f7d9e16941ddc48e8edf96b392e4df7
Vincenzo- <viulenzgg@gmail.com>
2022-07-16 13:54:05 +0200

    Initial commit + Exercise 1 using checkboxes, array v-model

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="">
+    <div class="price-query">
+      <h2>What would you like to do?</h2>
+      <div>
+        <label for="web">A web page - 500€</label>
+        <input type="checkbox" id="web" :value="500" v-model="prices" @change="calculateTotal" /> <br />
+      </div>
+
+      <div>
+        <label for="seo">SEO Consulting Service - 300€</label>
+        <input type="checkbox" id="seo" :value="300" v-model="prices" @change="calculateTotal" /> <br />
+      </div>
+
+      <div>
+        <label for="google">Google Ads Campaign - 200€</label>
+        <input type="checkbox" id="google" :value="200" v-model="prices" @change="calculateTotal" /> <br />
+      </div>
+    </div>
+  </form>
+
+  <h3 v-if="totalQuote">Total: {{ totalQuote }}€</h3>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      prices: [],
+      totalQuote: 0,
+    };
+  },
+
+  methods: {
+    calculateTotal() {
+      this.totalQuote = 0;
+      this.prices.forEach((price) => {
+        this.totalQuote += price;
+      });
+
+    },
+  },
+};
 </script>
+
+<style scoped>
+h3 {
+  color: teal;
+}
+</style>
