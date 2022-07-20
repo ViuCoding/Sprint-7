e40039b779ac4e0a598b7193fa3a244578040793
Vincenzo- <viulenzgg@gmail.com>
2022-07-20 10:10:47 +0200

    Added welcome page and custom toggle checkboxes. Exercise 4 box

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  
   <div v-if="showPage">
     <router-view />
   </div>
 
   <div v-else class="welcome">
-    <h1>WELCOME TO PRESUPESTOS.COM</h1>
+    <h1>WELCOME TO <span class="highlight">PRESUPESTOS</span>.COM</h1>
     <div class="box">
-      <p>
+      <p class="msg">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, eveniet adipisci? Nam repellendus aliquid eveniet totam iste esse, perferendis omnis corporis, ab fuga, natus sit dolor vel hic dignissimos quo enim assumenda. Sed magnam fugit veniam facere suscipit ea debitis voluptatem placeat maxime qui asperiores itaque atque similique incidunt quis sequi error, nulla quod quisquam quae! Necessitatibus tenetur molestiae consectetur velit ipsam doloremque dolorum consequuntur
         quisquam sed nemo, accusamus odio animi! Exercitationem modi error incidunt in ut nesciunt soluta esse vel alias! Omnis unde, quo consectetur, corrupti quos adipisci, culpa et ut neque iusto nobis laborum dolore. Omnis, dolorum blanditiis!
       </p>
     </div>
+    <div class="cta">
+      <h2><span>“Make it work, make it right, make it fast.”</span> – Kent Beck</h2>
+    </div>
     <div class="box">
-      <p>
+      <p class="msg">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, eveniet adipisci? Nam repellendus aliquid eveniet totam iste esse, perferendis omnis corporis, ab fuga, natus sit dolor vel hic dignissimos quo enim assumenda. Sed magnam fugit veniam facere suscipit ea debitis voluptatem placeat maxime qui asperiores itaque atque similique incidunt quis sequi error, nulla quod quisquam quae! Necessitatibus tenetur molestiae consectetur velit ipsam doloremque dolorum consequuntur
         quisquam sed nemo, accusamus odio animi! Exercitationem modi error incidunt in ut nesciunt soluta esse vel alias! Omnis unde, quo consectetur, corrupti quos adipisci, culpa et ut neque iusto nobis laborum dolore. Omnis, dolorum blanditiis!
       </p>
@@ -52,9 +54,7 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-.panel {
-  padding: 30px;
-}
+
 .panel a {
   font-weight: bold;
   padding: 0.8rem;
@@ -66,10 +66,28 @@ export default {
 
 .welcome {
   margin: 0 auto;
-  width: 60%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+
+.cta {
+  background: #42b983;
+  color: white;
+  width: 100%;
+}
+
+.cta span {
+  font-style: italic;
+}
+
+.msg {
+  margin: 2rem auto;
+  width: 60%;
+}
+
+.highlight {
+  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,10 @@
       <h1>What would you like to do?</h1>
       <div class="input-box">
         <label for="web">Web Page <span class="prices">500€</span></label>
-        <input type="checkbox" id="web" :value="500" v-model="prices" @change="calculateTotal" /> <br />
-        <div v-if="prices.includes(500)" class="panel">
-          <Panel @updatePages="updateNumPages" @updateLangs="updateNumLangs" />
-        </div>
+        <div class="center"><input type="checkbox" id="web" :value="500" v-model="prices" @change="calculateTotal" /> <br /></div>
+      </div>
+      <div v-if="prices.includes(500)" class="panel">
+        <Panel @updatePages="updateNumPages" @updateLangs="updateNumLangs" />
       </div>
 
       <div class="input-box">
@@ -80,7 +80,46 @@ label {
 
 .input-box {
   margin: 3rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+
+input[type="checkbox"] {
+  cursor: pointer;
+  position: relative;
+  width: 60px;
+  height: 30px;
+  appearance: none;
+  background: #2c3e50;
+  outline: none;
+  border-radius: 20px;
+  transition: 0.5s;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+}
+
+input:checked[type="checkbox"] {
+  background: #42b983;
+}
+
+input[type="checkbox"]:before {
+  content: "";
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 20px;
+  top: 0;
+  left: 0;
+  background: #fff;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+  transform: scale(1.1);
+  transition: 0.5s;
+}
+
+input:checked[type="checkbox"]:before {
+  left: 30px;
+}
+
 .prices {
   background: #2c3e50;
   color: white;
